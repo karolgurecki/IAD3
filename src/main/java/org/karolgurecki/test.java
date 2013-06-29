ddcdf595d7e9239360ec6658aaca406b1b74ef55
Karol Górecki <karolgurecki@gmail.com>
2013-06-29 16:59:08 +0200

    Testing IntelliJ Idea with Git

---
+++ src/main/java/org/karolgurecki/test.java
@@ -0,0 +1,11 @@
+package org.karolgurecki;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GodDamnItNappa!
+ * Date: 29.06.13
+ * Time: 16:58
+ * To change this template use File | Settings | File Templates.
+ */
+public class test {
+}
